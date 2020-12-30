0aac65d249066eb155cacc0e9ae961336e0fed93
kumankoishi.py <7iggif@naver.com>
2020-12-30 17:08:43 +0900

    Make Night Button available

--- Function/ui.js
+++ Function/ui.js
@@ -8,3 +8,39 @@ function GenerateMenus () {
     GenerateListItem("HowToCare.html", "애기코이시 돌보는 방법");
     GenerateListItem("BabyKoishiArt.html", "애기코이시 아트");
 }
+
+var Link = {
+    "SetColor": function (color) {
+        $('a').css("color", color);
+    }
+}
+
+var Body = {
+    "SetColor": function (color) {
+        $('body').css("color", color);
+    },
+    "SetBackgroundColor": function (color) {
+        $('body').css("backgroundColor", color);
+    }
+}
+
+var Theme = {
+    "isNight": false,
+    "NightDayInvert": function (button) {
+        if (this.isNight) {
+            // if night theme -> day
+            Body.SetColor('black');
+            Body.SetBackgroundColor('white');
+            Link.SetColor('#20CC20');
+            this.isNight = false;
+            button.value = "NIGHT";
+        } else {
+            // if day theme -> night
+            Body.SetColor('white');
+            Body.SetBackgroundColor('black');
+            Link.SetColor('#00DD00');
+            this.isNight = true;
+            button.value = "DAY";
+        }
+    }
+}

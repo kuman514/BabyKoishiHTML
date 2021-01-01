9e96e104044f0f079be02b0c12fdecd01618f412
kumankoishi.py <7iggif@naver.com>
2021-01-02 00:26:52 +0900

    Separate and add functions

--- Function/ui.js
+++ Function/ui.js
@@ -1,14 +1,3 @@
-function GenerateListItem (ref, title) {
-    document.write("<li><a href='" + ref + "'>" + title + "</a></li>");
-}
-
-function GenerateMenus () {
-    GenerateListItem("WhoIsKoishi.html", "코이시는 누구인가요?");
-    GenerateListItem("WhoIsBabyKoishi.html", "애기코이시는 어떤 코이시인가요?");
-    GenerateListItem("HowToCare.html", "애기코이시 돌보는 방법");
-    GenerateListItem("BabyKoishiArt.html", "애기코이시 아트");
-}
-
 var Link = {
     "SetColor": function (color) {
         $('a').css("color", color);

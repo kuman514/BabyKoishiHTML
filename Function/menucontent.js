84c11dd17dc8876a0e38acea7b083e201d5eb2a5
kumankoishi.py <7iggif@naver.com>
2021-01-10 17:29:38 +0900

    Solving errors applying AJAX

--- Function/menucontent.js
+++ Function/menucontent.js
@@ -1,10 +1,10 @@
 function GenerateListItem (ref, title) {
-  document.write("<li><a onclick='changeArticle('article/" + ref + "')'>" + title + "</a></li>")
+  document.write("<li><a onclick='changeArticle(\"" + ref + "\")'>" + title + "</a></li>")
 }
 
 function GenerateMenus () {
-  GenerateListItem("WhoIsKoishi.html", "코이시는 누구인가요?")
-  GenerateListItem("WhoIsBabyKoishi.html", "애기코이시는 어떤 코이시인가요?")
-  GenerateListItem("HowToCare.html", "애기코이시 돌보는 방법")
-  GenerateListItem("BabyKoishiArt.html", "애기코이시 아트")
+  GenerateListItem("article/WhoIsKoishi.html", "코이시는 누구인가요?")
+  GenerateListItem("article/WhoIsBabyKoishi.html", "애기코이시는 어떤 코이시인가요?")
+  GenerateListItem("article/HowToCare.html", "애기코이시 돌보는 방법")
+  GenerateListItem("article/BabyKoishiArt.html", "애기코이시 아트")
 }
